bc7e46a3efd97b1ff301443767ea5f2c6803233d
kimyh5910 <59380669+kimyh5910@users.noreply.github.com>
2022-12-13 13:50:52 +0900

    Add files via upload

---
+++ day16/h/Main3.java
@@ -0,0 +1,19 @@
+package com.kh.day16.h;
+
+public class Main3 {
+  public static void main(String[] args) {
+    String str1 = "가나다";
+    String str3 = "가다";
+
+    System.out.println(str1.compareTo(str1));
+
+
+    int x = 10;
+    int y = 20;
+
+    Integer x2 = x;
+    Integer y2 = y;
+
+    System.out.println(x2.compareTo(y2));
+  }
+}
