f15894935d4f2bcd644d8c6a4bfd9d79928d502b
kimyh5910 <59380669+kimyh5910@users.noreply.github.com>
2022-12-13 13:45:44 +0900

    Add files via upload

---
+++ day7/Test10.java
@@ -0,0 +1,31 @@
+//package com.kh.day7;
+//
+//public class Test10 {
+//  public static void main(String[] args) {
+//
+//
+//
+////
+////
+////
+////    int seatNum = 0;
+////    for (int i = 0; i < seats.length; i++) {
+////      for (int j = 0; j < seats[i].length; j++) {
+////        seats[i][j] = ++seatNum;
+////      }
+////    }
+////    System.out.println("좌석 선택");
+////    String[][] seats = new String[6][10];
+////    // 좌석 출력
+////    for (int i = 0; i < seats.length; i++) {
+////      for (int j = 0; j < seats[i].length; j++) {
+////        System.out.print(seats[i][j] + " ");
+////      }
+////      System.out.println();
+////    }
+////
+////
+////
+////
+////    }
+////  }
---
+++ day7/Test9.java
@@ -0,0 +1,14 @@
+package com.kh.day7;
+
+//import java.lang.*;   // 자동으로 해줌
+
+
+public class Test9 {
+  public static void main(String[] args) {
+    System.out.println("hello");
+    String name = "홍길동";
+
+//    Arrays.sort(new int[]{1,2,3});
+//
+  }
+}
