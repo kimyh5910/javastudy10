c5b05fd20e900de7f7759ea63e9d7b4b4820b69a
kimyh5910 <59380669+kimyh5910@users.noreply.github.com>
2022-12-13 13:45:18 +0900

    Add files via upload

---
+++ day6/Test12.java
@@ -0,0 +1,15 @@
+package com.kh.day6;
+
+import java.util.Scanner;
+
+public class Test12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("학생 성적 분석");
+        System.out.print("과목수");
+        int numOfSubjects = Integer.parseInt(scanner.nextLine());
+
+
+
+    }
+}
